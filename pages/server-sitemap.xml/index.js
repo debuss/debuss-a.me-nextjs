0f5f19f5246329763a34df538c771c6d7281343d
Alexandre Debusschère <alexandre.debusschere@hey.com>
2022-11-21 11:14:35 +0100

    Fix: sitemap

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,12 +1,12 @@
 import { getServerSideSitemap } from 'next-sitemap';
 
 export const getServerSideProps = async (ctx) => {
-    const response = await fetch('https://strapi.debuss-a.me/articles?_sort=id:desc');
+    const response = await fetch('https://strapi-production-4cf6.up.railway.app/api/articles?sort=id:desc&fields[0]=slugurl&fields[1]=published');
     const posts = await response.json();
 
-    const fields = posts.filter(post => post.active).map(post => ({
-        loc: `https://debuss-a.me/blog/${post.slugurl}`,
-        lastmod: new Date(post.published).toISOString(),
+    const fields = posts?.data?.map(post => ({
+        loc: `https://debuss-a.me/blog/${post?.attributes?.slugurl}`,
+        lastmod: new Date(post?.attributes?.published).toISOString(),
         changefreq: 'yearly',
         priority: 0.5
     }));
